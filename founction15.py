bfa1e96e14353697c8456b51a38ca8ab2f9cb73b
بهاره نوریان <bahar.coder3@gmail.com>
2026-01-03 17:00:17 +0330

    حذف اعداد منفی

---
+++ founction15.py
@@ -0,0 +1,11 @@
+
+#حذف اعداد منفی
+
+def numbers(a):
+    new_list=[]
+    for i in a:
+        if i>0:
+            new_list.append()
+            return new_list
+        result=numbers()
+        print(result)
